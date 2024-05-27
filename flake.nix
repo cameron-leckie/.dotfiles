68351da91ba7f0cc5ca2e3e8b2f78f078956100c
cameron-leckie <cdl.public@proton.me>
2024-05-27 07:30:55 +0100

    First home manager commit

--- flake.nix
+++ flake.nix
@@ -4,17 +4,26 @@
     
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
     let
       lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       CL-FW13 = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
-      
+        inherit system;
+        modules = [ ./configuration.nix ]; 
+      };
+    };
+    homConfigurations = {
+      CL-FW13 = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ]; 
       };
     };
   };
