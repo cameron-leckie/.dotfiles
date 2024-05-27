a57cc0ca845b680c75b118b6656a2fca6ac57aff
cameron-leckie <cdl.public@proton.me>
2024-05-27 21:25:14 +0100

    removed brave

--- configuration.nix
+++ configuration.nix
@@ -139,7 +139,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    brave 
+  
   ];
   
   nixpkgs.config.permittedInsecurePackages = [
