6ad98267adf58599ef0ece767932751fffe81859
cameron-leckie <cdl.public@proton.me>
2024-05-28 23:13:39 +0100

    half change to office in home.nix

--- home.nix
+++ home.nix
@@ -17,40 +17,47 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     
+    ##############
+    ### Office ###
+    ##############
+    libreoffice
+    # pkgs.obsidian
+    # pkgs.zoom-us
+
     #########################
     ### Basic Development ###
     #########################
-    pkgs.python311
-    pkgs.gnuplot
+    python311
+    gnuplot
     # pkgs.g3data
     # pkgs.zed-editor # waiting for stable release
 
     ##################
     ### Networking ###
     ##################
-    pkgs.vpnc # For strath vpn
+    vpnc # For strath vpn
     
     #################
     ### Aesthetic ###
     #################
-    pkgs.fastfetch
+    fastfetch
     
     # Adds the 'hello' command to your environment. It prints a friendly
     # "Hello, world!" when run.
-    pkgs.hello
+    hello
 
     # It is sometimes useful to fine-tune packages, for example, by applying
     # overrides. You can do that directly here, just don't forget the
     # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # fonts?
-    (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
     # You can also create simple shell scripts directly inside your
     # configuration. For example, this adds a command 'my-hello' to your
     # environment:
-    (pkgs.writeShellScriptBin "my-hello" ''
+    (writeShellScriptBin "my-hello" ''
       echo "Hello, ${config.home.username}!"
     '')
     
