f82e4b2d0608c8936a6aff9d1f88d231e750bb6a
cameronleckie <cdl.public@proton.me>
2024-09-11 16:27:28 +0100

    latest version - don't know difference

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
-      nixos = lib.nixosSystem {
+      CL-FW13  = lib.nixosSystem {
         inherit system;
         modules = [ ./configuration.nix ]; 
       };
