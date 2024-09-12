258d329fc7c66d7c17f63576babce38aad714e94
cameron-leckie <cdl.public@proton.me>
2024-09-12 11:53:11 +0100

    cleaned up hardware files

--- FW13/hardware-configuration.nix
+++ FW13/hardware-configuration.nix
@@ -8,34 +8,38 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/00c36cf4-e2ab-44dc-9986-291981c7317d";
+    { device = "/dev/disk/by-uuid/976e56aa-113a-47bc-9ba7-40d38977bd3f";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/8E29-6A6A";
+    { device = "/dev/disk/by-uuid/B818-7B40";
       fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/d8b15f15-51c2-47b4-9118-9b39979bbb3a"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp170s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Firmwear updater
+  services.fwupd.enable = true;
+  
+  # Power management packages
+  powerManagement.enable = true; # Allows nixos to control hibernate and suspend
+  services.thermald.enable = true; # Controls CPU thermal - good for intel?
 }
